665f97a8a1e5b6f72d8db4041446789fb8a0a1a9
Boris Finkelshteyn <finke.ba@gmail.com>
2019-10-20 17:12:38 +0300

    Stack Traces with Reactor Tools.

--- src/main/java/com/jug/webflux/showcase/WebfluxShowcaseApplication.java
+++ src/main/java/com/jug/webflux/showcase/WebfluxShowcaseApplication.java
@@ -3,12 +3,14 @@ package com.jug.webflux.showcase;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import reactivefeign.spring.config.EnableReactiveFeignClients;
+import reactor.tools.agent.ReactorDebugAgent;
 
 @SpringBootApplication
 @EnableReactiveFeignClients
 public class WebfluxShowcaseApplication {
 
     public static void main(String[] args) {
+        ReactorDebugAgent.init();
         SpringApplication.run(WebfluxShowcaseApplication.class, args);
     }
 
